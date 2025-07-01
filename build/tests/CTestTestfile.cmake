b3fe52629c2047ee55fe6dbe3c6653c80cebc7dc
life1176ac <life1176ac@gmail.com>
2025-07-02 00:52:28 +0900

    test binary

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/life1/dev/BoxelGame/tests
+# Build directory: /home/life1/dev/BoxelGame/build/linux-debug/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
