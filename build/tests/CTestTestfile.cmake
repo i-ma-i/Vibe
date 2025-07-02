49e581fd0e53c68b4ae58287659866ad132902c7
life1176ac <life1176ac@gmail.com>
2025-07-02 19:53:52 +0900

    commit

--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/life1/dev/BoxelGame/tests
-# Build directory: /home/life1/dev/BoxelGame/build/linux-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
---
+++ developmant.md
@@ -0,0 +1,8 @@
+```plaintext
+git clone https://github.com/i-ma-i/BoxelGame.git
+cd BoxelGame
+./script/setup-ubuntu.sh
+./script/build.sh
+./scripts/test.sh
+
+```
